5a133f715c24afb5f4556eb4f098de5266b3cf71
Matthew Justice <matthewajustice@gmail.com>
2020-11-13 11:56:30 -0500

    2/4 topics complete

--- index.js
+++ index.js
@@ -87,7 +87,6 @@ function USApop(arr) {
   return totalPop;
 }
   
-  
   // 🦁🦁🦁 Callbacks 🦁🦁🦁  
   /* 🦁🦁🦁 Step 1: Create a higher-order function 🦁🦁🦁
     * Create a higher-order function named consume with 3 parameters: a, b and cb
@@ -95,25 +94,29 @@ function USApop(arr) {
     * The last parameter accepts a callback
     * The consume function should return the invocation of cb, passing a and b into cb as arguments
   */
-  function consume(/*Your Code Here */){
-    /*Your Code Here */
-  }
+
+function consume(a, b, cb) {
+  return cb(a, b);
+}
  
-  
   /* 🦁🦁🦁 Step 2: Create several functions to callback with consume(); 🦁🦁🦁 */
  // 🦁🦁🦁 Create a function named add that returns the sum of two numbers 🦁🦁🦁
-  function add(/*Your Code Here */){
-    /*Your Code Here*/
-  }
+
+function add(a, b) {
+  return a + b;
+}
+
 // 🦁🦁🦁 Create a function named multiply that returns the product of two numbers 🦁🦁🦁
-  function multiply(/*Your Code Here */){
-   /*Your Code Here */
-  }
+
+function multiply(a, b) {
+  return a * b;
+}
 
  // 🦁🦁🦁 Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!" 🦁🦁🦁
-  function greeting(/*Your Code Here */){
-   return /*Your Code Here */
-  }
+
+function greeting(a, b) {
+  return `Hello ${a} ${b}, nice to meet you!`
+}
   
   // 🦁🦁🦁 Step 3: Check your work by un-commenting the following calls to consume(): 🦁🦁🦁 
   // ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️
